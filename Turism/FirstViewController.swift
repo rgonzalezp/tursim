bbd8cdc51a8a4a2ecdb4f564cc285a8e1a103c07
Federico Gonzalez <rgonzalezp1115@gmail.com>
2019-02-08 10:34:17 -0500

    Initial Commit

---
+++ Turism/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Turism
+//
+//  Created by Federico Gonzalez on 2/8/19.
+//  Copyright © 2019 Ricardo Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Turism/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Turism
+//
+//  Created by Federico Gonzalez on 2/8/19.
+//  Copyright © 2019 Ricardo Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
